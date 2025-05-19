47e5eddf0f9d5c5942d36f6e43879c8a9f6e5bf3
Kiel Bermudez <149242693+kiel24k@users.noreply.github.com>
2025-05-19 18:11:50 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Greet from './components/Greet'
+
+function App() {
+ 
+
+  return (
+    <div className="app">
+      <Greet/>
+      <h1>Hello world</h1>
+    </div>
+  ) 
+}
+
+export default App
